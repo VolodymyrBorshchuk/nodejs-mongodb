03354955a5a74267a4d09f53d761388db5ca737f
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2023-04-22 12:15:52 +0300

    basic knowledge of events

--- log.js
+++ log.js
@@ -1,13 +1,10 @@
 const EventEmitter = require('events');
-const util = require('util');
 
-class Logger {
+class Logger extends EventEmitter {
     log = (msg) => {
         console.log(msg)
         this.emit('some_event', { id: 1, text: 'some event text' });
     }
 }
-
-util.inherits(Logger, EventEmitter)
 
 module.exports = Logger;
