2c51f5ba9a37dc9d71cad81bb09c46473e3a0e15
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2023-04-26 16:51:34 +0300

    EJS, dynamic pages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express();
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 
 app.listen(PORT, (error) => {
@@ -12,10 +12,29 @@ app.listen(PORT, (error) => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'));
+    const title = 'Home Page'
+    res.render(createPath('index'), {title});
 })
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'));
+    const title = 'Contacts'
+    const contacts = [
+        { name: 'LinkedIn', link: 'https://www.linkedin.com/in/volodymyr-borshchuk-425301263/' },
+        { name: 'Instagram', link: 'https://www.instagram.com/volodymyrborshuk/' },
+        { name: 'Facebook', link: 'https://www.facebook.com/profile.php?id=100004497062255' },
+    ]
+    res.render(createPath('contacts'), {contacts, title});
+})
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post'
+    res.render(createPath('post'), {title});
+})
+app.get('/posts', (req, res) => {
+    const title = 'Posts'
+    res.render(createPath('posts'), {title});
+})
+app.get('/add-post', (req, res) => {
+    const title = 'New Post'
+    res.render(createPath('add-post'), {title});
 })
 
 app.get('/about-us', (req, res) => {
@@ -23,7 +42,8 @@ app.get('/about-us', (req, res) => {
 })
 
 app.use((req, res) => {
+    const title = 'Error Page'
     res
         .status(404)
-        .sendFile(createPath('error'))
+        .render(createPath('error'), {title})
 })
