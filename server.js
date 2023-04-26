1869d5fb5c9903aaf069deb1ec0ec606e7bfad68
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2023-04-26 18:21:30 +0300

    middleware & static files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
@@ -11,9 +12,12 @@ app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`Listening Port ${PORT}`)
 })
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.static('styles'))
+
 app.get('/', (req, res) => {
     const title = 'Home Page'
-    res.render(createPath('index'), {title});
+    res.render(createPath('index'), { title });
 })
 app.get('/contacts', (req, res) => {
     const title = 'Contacts'
@@ -22,19 +26,19 @@ app.get('/contacts', (req, res) => {
         { name: 'Instagram', link: 'https://www.instagram.com/volodymyrborshuk/' },
         { name: 'Facebook', link: 'https://www.facebook.com/profile.php?id=100004497062255' },
     ]
-    res.render(createPath('contacts'), {contacts, title});
+    res.render(createPath('contacts'), { contacts, title });
 })
 app.get('/posts/:id', (req, res) => {
     const title = 'Post'
-    res.render(createPath('post'), {title});
+    res.render(createPath('post'), { title });
 })
 app.get('/posts', (req, res) => {
     const title = 'Posts'
-    res.render(createPath('posts'), {title});
+    res.render(createPath('posts'), { title });
 })
 app.get('/add-post', (req, res) => {
     const title = 'New Post'
-    res.render(createPath('add-post'), {title});
+    res.render(createPath('add-post'), { title });
 })
 
 app.get('/about-us', (req, res) => {
@@ -45,5 +49,5 @@ app.use((req, res) => {
     const title = 'Error Page'
     res
         .status(404)
-        .render(createPath('error'), {title})
+        .render(createPath('error'), { title })
 })
